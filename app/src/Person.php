47972ca58a3fe5dc988708cffb4925df13748304
vasilyev <vasilyev@ugol.me>
2019-12-22 16:55:21 +0300

    Packagist package init

---
+++ app/src/Person.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AlexanderVasiliev23;
+
+/**
+ * Class Person
+ * @package AlexandeVasiliev23\Person
+ */
+class Person
+{
+    private string $name;
+
+    /**
+     * Person constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function greet(): string
+    {
+        return 'Hello, ' . $this->name;
+    }
+}
